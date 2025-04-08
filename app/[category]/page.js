975bf7b1962063dc1c0166a14808fce521758957
ortalyarts <148264882+ortalyarts@users.noreply.github.com>
2025-04-08 16:52:40 +0200

    update category page

--- app/[category]/page.js
+++ app/[category]/page.js
@@ -1,11 +1,10 @@
+// import { notFound } from 'next/navigation';
 import { Suspense } from 'react';
-import { notFound } from 'next/navigation';
 
 import { getProducts } from '@/lib/util.js';
 
 import ThumbProductCategories from "@/components/thumbProductCategories";
 import Article from "@/components/article";
-
 import ProductListItem from "@/components/productListItem";
 
 const VALID_CATEGORIES = ['headphones', 'speakers', 'earphones'];
@@ -13,37 +12,36 @@ const VALID_CATEGORIES = ['headphones', 'speakers', 'earphones'];
 export async function generateStaticParams() {
   return VALID_CATEGORIES.map(category => ({ category }));
 }
-export const dynamicParams = false;
 
-export default async function Category(props) {
-  const products = getProducts();
-  const params = await props.params;
+export const dynamicParams = false; // Optional: disables fallback mode
 
-  const selectedCategory = await params.category;
+export default async function Category({ params }) {
+  const selectedCategory = params.category;
+
   if (!VALID_CATEGORIES.includes(selectedCategory)) {
-    notFound(); // triggers Next.js's 404 page
+    notFound();
   }
-  const productsOfCategory = products.filter(item => item.category == selectedCategory);
+
+  const products = getProducts(); // if this is sync, no need to await
+  const productsOfCategory = products.filter(item => item.category === selectedCategory);
 
   return (
     <>
-    <div className='category-name'>
-      <h1 className='title-3'>{selectedCategory}</h1>
-    </div>
-    <div className="content-holder">
-    <Suspense fallback={<p>Loading products...</p>}>
-      <div className="list-of-products">
-        {productsOfCategory.map((item) => {
-          const itemImages = productsOfCategory.filter(set => set.product_id === item.id);
-          return <ProductListItem key={item.slug} item={item} itemImages={itemImages}/>
-        })}
-
+      <div className='category-name'>
+        <h1 className='title-3'>{selectedCategory}</h1>
       </div>
-    </Suspense>
-
-    <ThumbProductCategories />
-    <Article />
-  </div>
-  </>
+      <div className="content-holder">
+        <Suspense fallback={<p>Loading products...</p>}>
+          <div className="list-of-products">
+            {productsOfCategory.map((item) => {
+              const itemImages = productsOfCategory.filter(set => set.product_id === item.id);
+              return <ProductListItem key={item.slug} item={item} itemImages={itemImages} />
+            })}
+          </div>
+        </Suspense>
+        <ThumbProductCategories />
+        <Article />
+      </div>
+    </>
   );
 }
