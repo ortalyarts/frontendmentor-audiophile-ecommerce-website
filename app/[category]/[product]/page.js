edfccb8c0bd43f01cefc9f045999de48ec0dffe3
ortalyarts <148264882+ortalyarts@users.noreply.github.com>
2025-04-17 21:08:11 +0200

    v 17

--- app/[category]/[product]/page.js
+++ app/[category]/[product]/page.js
@@ -1,7 +1,25 @@
 
+import { getCategoriesWithProducts } from '@/lib/util.js';
 
+export async function generateStaticParams() {
+    const categoriesWithProducts = await getCategoriesWithProducts();
+  
+    const params = [];
+    categoriesWithProducts.forEach(({ category, products }) => {
+      products.forEach(product => {
+        params.push({
+          category,
+          product: product.slug,
+        });
+      });
+    });
+    console.log('Generated static params:', params); // Debugging output
+    return params;
+  }
 export default async function ProductDetails(props) {
-
+    (async () => {
+        generateStaticParams()
+      })();
  
     return (
     <h1>Test page</h1>
