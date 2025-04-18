20585e3dee8977a91371c6e468d538ea203b916b
ortalyarts <148264882+ortalyarts@users.noreply.github.com>
2025-04-18 10:47:16 +0200

    v23

--- components/logoAndNav.jsx
+++ components/logoAndNav.jsx
@@ -21,7 +21,7 @@ export default function LogoAndNav (){
   function handleCloseNav(){
       setIsOpen(false)
   }
-console.log('V 22')
+console.log('V 23')
     return (
     <div className="nav-and-logo">
         <Link className="logo" onClick={toggleMenu} href="/"><Image src='/assets/shared/desktop/logo.svg' priority alt="Audiophile logo" width="143" height="25"/></Link>
