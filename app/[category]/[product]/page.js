873f3baabbb91167d356816c538687f923e47091
ortalyarts <148264882+ortalyarts@users.noreply.github.com>
2025-04-18 10:12:43 +0200

    v 19 hard written StaticParams

--- app/[category]/[product]/page.js
+++ app/[category]/[product]/page.js
@@ -1,19 +1,31 @@
 
 import { getCategoriesWithProducts } from '@/lib/util.js';
 
-export async function generateStaticParams() {
-    const categoriesWithProducts = await getCategoriesWithProducts();
+// export async function generateStaticParams() {
+//     const categoriesWithProducts = await getCategoriesWithProducts();
   
-    // Flatten the structure into an array of { category, product } objects
-    const params = categoriesWithProducts.flatMap(({ category, products }) =>
-        products.map(product => ({
-            category,
-            product: product.slug,
-        }))
-    );
-    console.log('Generated static params:', params); // Debugging output
-    return params;
-  }
+//     // Flatten the structure into an array of { category, product } objects
+//     const params = categoriesWithProducts.flatMap(({ category, products }) =>
+//         products.map(product => ({
+//             category,
+//             product: product.slug,
+//         }))
+//     );
+//     console.log('Generated static params:', params); // Debugging output
+//     return params;
+//   }
+
+export async function generateStaticParams() {
+    return [
+        { category: 'earphones', product: 'yx1-earphones' },
+        { category: 'headphones', product: 'xx59-headphones' },
+        { category: 'headphones', product: 'xx99-mark-one-headphones' },
+        { category: 'headphones', product: 'xx99-mark-two-headphones' },
+        { category: 'speakers', product: 'zx7-speaker' },
+        { category: 'speakers', product: 'zx9-speaker' },
+    ];
+}
+
 export default async function ProductDetails(props) {
  
     return (
